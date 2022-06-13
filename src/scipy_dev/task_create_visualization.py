2deb48ed16cc9bcf8d8653f2cc8863f94a2f9a5d
timmens <tim@mensinger96.com>
2022-06-13 11:51:55 +0200

    Add tight layout

--- src/scipy_dev/task_create_visualization.py
+++ src/scipy_dev/task_create_visualization.py
@@ -8,12 +8,14 @@ from scipy_dev.visualizations import create_grid_search_figure
 @pytask.mark.produces(BLD.joinpath("figures", "grid_search.png"))
 def task_create_grid_search_figure(produces):
     fig, _ = create_grid_search_figure(contour_line_width=3)
+    fig.tight_layout()
     fig.savefig(produces)
 
 
 @pytask.mark.produces(BLD.joinpath("figures", "gradient_descent.png"))
 def task_create_gradient_descent_figure(produces):
     fig, _ = create_gradient_descent_figure(contour_line_width=3)
+    fig.tight_layout()
     fig.savefig(produces)
 
 
